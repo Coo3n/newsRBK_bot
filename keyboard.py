157f08d630b2e5a0d36e21ff897733dfd6d7861f
Coo3n <Khlamov_ilya@mail.ru>
2022-04-24 21:21:49 +0300

    first merge

--- keyboard.py
+++ keyboard.py
@@ -6,9 +6,7 @@ but_exit   = KeyboardButton("🔙Главное меню")
 but_polit  = KeyboardButton("💼Политика")
 but_econom = KeyboardButton("📉Экономика")
 but_sport  = KeyboardButton("🏋Спорт")
-but_crypt  = KeyboardButton("💶Бизнес")
-but_society  = KeyboardButton("🧑‍🤝‍🧑Общество")
 
 
-main_menu  = ReplyKeyboardMarkup(resize_keyboard=True).insert(but_polit).insert(but_econom).insert(but_sport).insert(but_crypt).insert(but_society)
+main_menu  = ReplyKeyboardMarkup(resize_keyboard=True).insert(but_polit).insert(but_econom).insert(but_sport)
 other_menu = ReplyKeyboardMarkup(resize_keyboard=True).insert(but1).insert(but2).add(but_exit)
--- main.py
+++ main.py
@@ -13,14 +13,14 @@ from aiogram.utils.exceptions import Throttled
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dispetcher = Dispatcher(bot,storage = storage)
-finish_str = " "
-
+finish_str = ""
+f = ""
 async def anti_flood(*args, **kwargs):
     ans = args[0]
     await ans.answer("Прекрати ломать бота, кликай спокойней")
 
 @dispetcher.message_handler(commands=['start'])
-@dispetcher.throttled(anti_flood,rate=2)
+@dispetcher.throttled(anti_flood, rate=1)
 async def process_start_command(message: types.Message):
     await message.reply("Привет!", reply_markup = kb.main_menu)
 
@@ -28,14 +28,14 @@ async def process_start_command(message: types.Message):
 async def process_help_command(message: types.Message):
     await message.reply("Данный Бот предоставляет услуги новостного канала! Чтобы получить новость - нажмите на одну из предложенных кнопок!")
 
-@dispetcher.message_handler(Text(equals="📰Текущие новости"))
-@dispetcher.throttled(anti_flood,rate=2)
-async def parse_five_news(message: types.Message):
-    choice_rubric(finish_str, "Текущие новости")
+@dispetcher.message_handler(Text(equals="🆕Последняя новость"))
+@dispetcher.throttled(anti_flood, rate=2)
+async def get_last_new(message: types.Message):
+    choice_rubric(finish_str,"Последняя новость")
     cnt_str = 1
     for news in result_list:
         if cnt_str == 2:
-            res = "🔥 " + title + '\n' + news
+            res = title + '\n' + news
             await message.answer(res)
             cnt_str = 1
         elif cnt_str == 1:
@@ -43,14 +43,14 @@ async def parse_five_news(message: types.Message):
             cnt_str+=1
     result_list.clear()
 
-@dispetcher.message_handler(Text(equals="🆕Последняя новость"))
-@dispetcher.throttled(anti_flood,rate=1)
-async def parse_last_news(message: types.Message):
-    choice_rubric(finish_str, "Последняя новость")
+@dispetcher.message_handler(Text(equals="📰Текущие новости"))
+@dispetcher.throttled(anti_flood, rate=1.5)
+async def get_lastFive_news(message: types.Message):
+    choice_rubric(finish_str,"Текущие новости")
     cnt_str = 1
     for news in result_list:
         if cnt_str == 2:
-            res = title + '\n' + news
+            res = "🔥 " + title + '\n' + news
             await message.answer(res)
             cnt_str = 1
         elif cnt_str == 1:
@@ -71,16 +71,12 @@ async def switch_menu(message: types.Message):
         case "🏋Спорт":
             finish_str = "Спорт"
             await message.answer("🏋Спорт", reply_markup=kb.other_menu)
-        case "💶Бизнес":
-            finish_str = "Бизнес"
-            await message.answer("💶Бизнес", reply_markup=kb.other_menu)
-        case "🧑‍🤝‍🧑Общество":
-            finish_str = "Общество"
-            await message.answer("🧑‍🤝‍🧑Общество", reply_markup=kb.other_menu)
         case "🔙Главное меню":
             await message.answer("🔙Главное меню", reply_markup=kb.main_menu)
         case _:
-            await message.answer("Введена неизвестная команда!" + '\n' + "Попробуйте написать команду: /help")
+            await message.answer("Введена неизвестная команда!" +'\n'+ "Попробуйте написать команду: /help")
+
+        
 
 if __name__ == '__main__':
-    executor.start_polling(dispetcher, skip_updates=True)
+    executor.start_polling(dispetcher,skip_updates=True)
