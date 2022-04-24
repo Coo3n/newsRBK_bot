35e06381a0f91c002ccdf8e846d3fc5ca3712c8f
Coo3n <Khlamov_ilya@mail.ru>
2022-04-24 21:18:08 +0300

    Upgrate speedBot

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.dispatcher.filters import Text
 from config import TOKEN
-from req import parse_site
-from req import choice_rubric
-from req import result_list
+from req import parse_site, choice_rubric, result_list
 import keyboard as kb
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.exceptions import Throttled
@@ -22,18 +20,45 @@ async def anti_flood(*args, **kwargs):
     await ans.answer("Прекрати ломать бота, кликай спокойней")
 
 @dispetcher.message_handler(commands=['start'])
-@dispetcher.throttled(anti_flood,rate=3)
+@dispetcher.throttled(anti_flood, rate=1)
 async def process_start_command(message: types.Message):
     await message.reply("Привет!", reply_markup = kb.main_menu)
 
 @dispetcher.message_handler(commands=['help'])
-@dispetcher.throttled(anti_flood,rate=3)
 async def process_help_command(message: types.Message):
     await message.reply("Данный Бот предоставляет услуги новостного канала! Чтобы получить новость - нажмите на одну из предложенных кнопок!")
 
+@dispetcher.message_handler(Text(equals="🆕Последняя новость"))
+@dispetcher.throttled(anti_flood, rate=2)
+async def get_last_new(message: types.Message):
+    choice_rubric(finish_str,"Последняя новость")
+    cnt_str = 1
+    for news in result_list:
+        if cnt_str == 2:
+            res = title + '\n' + news
+            await message.answer(res)
+            cnt_str = 1
+        elif cnt_str == 1:
+            title = news
+            cnt_str+=1
+    result_list.clear()
+
+@dispetcher.message_handler(Text(equals="📰Текущие новости"))
+@dispetcher.throttled(anti_flood, rate=1.5)
+async def get_lastFive_news(message: types.Message):
+    choice_rubric(finish_str,"Текущие новости")
+    cnt_str = 1
+    for news in result_list:
+        if cnt_str == 2:
+            res = "🔥 " + title + '\n' + news
+            await message.answer(res)
+            cnt_str = 1
+        elif cnt_str == 1:
+            title = news
+            cnt_str+=1
+    result_list.clear()
 
 @dispetcher.message_handler()
-@dispetcher.throttled(anti_flood,rate=2)
 async def switch_menu(message: types.Message):
     global finish_str
     match message.text:
@@ -46,31 +71,6 @@ async def switch_menu(message: types.Message):
         case "🏋Спорт":
             finish_str = "Спорт"
             await message.answer("🏋Спорт", reply_markup=kb.other_menu)
-        case "📰Текущие новости":
-            choice_rubric(finish_str,"Текущие новости")
-            cnt_str = 1
-            for news in result_list:
-                if cnt_str == 2:
-                    res = "🔥 " + title + '\n' + news
-                    await message.answer(res)
-                    cnt_str = 1
-                elif cnt_str == 1:
-                    title = news
-                    cnt_str+=1
-            result_list.clear()
-        case "🆕Последняя новость":
-            choice_rubric(finish_str,"Последняя новость")
-
-            cnt_str = 1
-            for news in result_list:
-                if cnt_str == 2:
-                    res = title + '\n' + news
-                    await message.answer(res)
-                    cnt_str = 1
-                elif cnt_str == 1:
-                    title = news
-                    cnt_str+=1
-            result_list.clear()
         case "🔙Главное меню":
             await message.answer("🔙Главное меню", reply_markup=kb.main_menu)
         case _:
